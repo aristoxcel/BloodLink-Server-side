1e62197600f2ac36a0355f029048ec7f51664130
Rakib Hasan <rakibnkhadija@gmail.com>
2024-06-04 00:20:30 +0600

    mongodb database added, userCollection created and response in post

--- index.js
+++ index.js
@@ -3,10 +3,44 @@ require ('dotenv').config();
 const express = require("express");
 const app = express();
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
+
+
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.Db_User}:${process.env.Db_Pass}@cluster0.kdbwfxu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+    try {
+
+        const userCollection = client.db('bloodBank').collection("users");
+        app.post('/user', async(req, res)=>{
+            const result = await userCollection.insertOne(req.body);
+            res.send(result)
+        })
+
+
+      await client.connect();
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+    //   await client.close();
+    }
+  }
+  run().catch(console.dir);
 
 app.get('/', (req, res)=>{
     res.send('this is home page')
